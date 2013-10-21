d2eea6df40c188530f5d780ee7800e6001bb4456
jgrn307 <jgrn307@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-21 20:34:31 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gdalutils@44 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gdalUtils_scratch/building_gdalUtils.R
+++ pkg/gdalUtils_scratch/building_gdalUtils.R
@@ -7,6 +7,9 @@ pathtopackage <- "/Users/jgrn307/Documents/workspace/gdalutils/pkg/gdalUtils"
 
 setwd(pathtopackage)
 
+# This builds the man pages and updates the NAMESPACE.
+# When in doubt, you can delete all your local man files
+# and this will re-create them.
 require("roxygen2")
 roxygenize(package.dir=pathtopackage,
 		roxygen.dir=pathtopackage,
