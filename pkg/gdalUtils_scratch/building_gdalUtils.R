93f245872b0e6c8059c059c81d38be8811cd06e7
jgrn307 <jgrn307@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-10-01 23:00:09 +0000

    Updated version number.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gdalutils@116 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gdalUtils_scratch/building_gdalUtils.R
+++ pkg/gdalUtils_scratch/building_gdalUtils.R
@@ -3,7 +3,7 @@ pathtopackage <- "D:\\Users\\jgrn\\workspace\\gdalutils\\pkg\\gdalUtils"
 
 # For jgrn/krypton:
 pathtopackage <- "/Users/jgrn307/Documents/workspace/gdalutils/pkg/gdalUtils"
-
+Sys.setenv(PATH=paste(Sys.getenv("PATH"),"/usr/texbin",sep=":"))
 
 setwd(pathtopackage)
 
@@ -11,10 +11,4 @@ setwd(pathtopackage)
 # When in doubt, you can delete all your local man files
 # and this will re-create them.
 require("roxygen2")
-<<<<<<< .mine
 roxygenize(package.dir=pathtopackage)
-=======
-roxygenize(package.dir=pathtopackage)
-
->>>>>>> .r108
-
