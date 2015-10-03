730c0bc4f9a0d1314f0c9efb82041c3ba609daa4
jgrn307 <jgrn307@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-10-03 21:54:59 +0000

    gdalmanage now working.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gdalutils@122 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gdalUtils_scratch/gdalUtils_examples.R
+++ pkg/gdalUtils_scratch/gdalUtils_examples.R
@@ -303,10 +303,46 @@ if(valid_install)
 	src_dataset <- system.file("external/tahoe_highrez.tif", package="gdalUtils")
 	# Raw output of a single coordinate:
 	gdallocationinfo(srcfile=src_dataset,x=10,y=10)
-	
+
 	# A matrix of coordinates and a clean, matrix output:
 	coords <- rbind(c(10,10),c(20,20),c(30,30))
 	gdallocationinfo(srcfile=src_dataset,coords=coords,valonly=TRUE,raw_output=FALSE)
 	
 	
+}
+
+
+#### gdalmanage:
+gdal_setInstallation()
+valid_install <- !is.null(getOption("gdalUtils_gdalPath"))
+if(valid_install)
+{
+	# Using identify mode
+	# Report the data format of the raster file by using the identify mode and specifying a data file name:
+	src_dataset <- system.file("external/tahoe_highrez.tif", package="gdalUtils")
+	gdalmanage(mode="identify",datasetname=src_dataset)
+	
+	# Recursive mode will scan subfolders and report the data format:	
+	src_dir <- system.file("external/", package="gdalUtils")
+	gdalmanage(mode="identify",datasetname=src_dir,r=TRUE)
+	
+	\dontrun{
+	# Using copy mode	
+	# Copy the raster data:
+	file_copy <- tempfile(fileext=".tif")
+	gdalmanage(mode="copy",src_dataset,file_copy)	
+	file.exists(file_copy)
+	
+	# Using rename mode
+	# Rename the raster data:
+	new_name <- tempfile(fileext=".tif")
+	gdalmanage(mode="rename",file_copy,new_name)	
+	file.exists(new_name)
+	
+	# Using delete mode
+	# Delete the raster data:
+	gdalmanage(mode="delete",new_name)	
+	file.exists(new_name)
+	
+	}
 }
