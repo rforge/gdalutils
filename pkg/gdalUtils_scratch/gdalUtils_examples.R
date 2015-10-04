efdee07e41de9a025be2694b6312a4b8c5197110
jgrn307 <jgrn307@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-10-04 00:08:07 +0000

    Almost ready for CRAN...
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gdalutils@126 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gdalUtils_scratch/gdalUtils_examples.R
+++ pkg/gdalUtils_scratch/gdalUtils_examples.R
@@ -358,3 +358,22 @@ if(require(rgdal) && valid_install)
 	ogrtindex(output_dataset=tempindex,src_dataset=src_files,
 			accept_different_schemas=TRUE,output_Vector=TRUE)
 }
+
+
+
+
+#### mosaic_rasters
+# We'll pre-check to make sure there is a valid GDAL install
+# and that raster and rgdal are also installed.
+# Note this isn't strictly neccessary, as executing the function will
+# force a search for a valid GDAL install.
+gdal_setInstallation()
+valid_install <- !is.null(getOption("gdalUtils_gdalPath"))
+if(require(raster) && require(rgdal) && valid_install)
+{
+	layer1 <- system.file("external/tahoe_lidar_bareearth.tif", package="gdalUtils")
+	layer2 <- system.file("external/tahoe_lidar_highesthit.tif", package="gdalUtils")
+	mosaic_rasters(gdalfile=c(layer1,layer2),dst_dataset="test_mosaic.envi",separate=TRUE,of="ENVI",
+			verbose=TRUE)
+	gdalinfo("test_mosaic.envi")
+}
