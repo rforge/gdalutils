ef165bf7a0876ec418816cc3130e42929a052807
jgrn307 <jgrn307@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-02-26 19:36:47 +0000

    Fixing documentation bugs.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gdalutils@140 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gdalUtils/R/is.Raster.R
+++ pkg/gdalUtils/R/is.Raster.R
@@ -4,12 +4,15 @@
 #' @return A logical vector.
 #' @author Jonathan A. Greenberg
 #' @examples
+#' if(require(raster) && require(rgdal))
+#' {
 #' tahoe_highrez <- brick(system.file("external/tahoe_highrez.tif", package="gdalUtils"))
 #' is.Raster(tahoe_highrez)
 #' tahoe_lidar_bareearth <- 
 #' 	raster(system.file("external/tahoe_lidar_bareearth.tif", package="gdalUtils"))
 #' is.Raster(tahoe_lidar_bareearth)
 #' is.Raster("character")
+#' }
 #' @import raster
 #' @export
 
